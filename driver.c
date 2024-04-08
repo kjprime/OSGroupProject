78555725152aa8fd2fc4591f77c2b2e3c724fa94
Kevin <ps2gamer20@gmail.com>
2024-04-07 21:57:32 -0500

    update driver.c with more pseudo code

--- driver.c
+++ driver.c
@@ -13,10 +13,27 @@ File Discription: Main function driver file for OS project
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ProcessManagment.c>
+#include <Syncronization.c>
 
 int main() {
-    char input_txt[] = "input.txt";
-    
+    char input_txt[] = "input.txt"; //input file name
+
+    //loop for input file content avalablity
+
+    // Process input text for data related to account functions
+
+    //Pass input perameters to procecss managment based uppon function needed.
+
+    //report progress
+
+    //execute deadlock prevention routine
+
+    //exit loop condition EOF
+
+    //wait for all processes to have finished
+
+    //report completion status
     
     // Exit the program
     return 0;
