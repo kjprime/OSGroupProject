ea1897b9fec9b564c8952873dde05df06735ae0e
Lucas Sager <157430606+LucasSager@users.noreply.github.com>
2024-04-10 19:46:24 -0500

    Update ProcessManagement.c

--- ProcessManagement.c
+++ ProcessManagement.c
@@ -9,51 +9,154 @@ Date:4/7/2024
 
 File Discription:
 */
+
+
 // Global data structure to hold account information.
-// All Sudo Code. Simply a representation of how the program will be laid out, but not including actual implementaion.
-AccountInfo = {}
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 
-// Function to create a new account with an initial balance of 0.
-Function CreateAccount(accountID):
-    AccountInfo[accountID] = 0
-    Print("Account created with ID:", accountID)
+typedef struct {
+    char accountID[10];
+    char transactionType[10];
+    int amount;
+    char targetAccountID[10]; // For transfer operations
+} Transaction;
 
-// Function to deposit money into an account.
-Function Deposit(accountID, amount):
-    If accountID not in AccountInfo:
-        Print("Account does not exist.")
-        Return
-    AccountInfo[accountID] += amount
-    Print(amount, "deposited into account ID:", accountID)
+// Transaction function prototypes
+void processTransaction(Transaction transaction);
+void createAccount(char* accountID, int initialBalance);
+void deposit(char* accountID, int amount);
+void withdraw(char* accountID, int amount);
+void transfer(char* accountID, char* targetAccountID, int amount);
+void inquiry(char* accountID);
+void closeAccount(char* accountID);
 
-// Function to withdraw money from an account.
-Function Withdraw(accountID, amount):
-    If accountID not in AccountInfo or AccountInfo[accountID] < amount:
-        Print("Insufficient funds or account does not exist.")
-        Return
-    AccountInfo[accountID] -= amount
-    Print(amount, "withdrawn from account ID:", accountID)
+int main() {
+    Transaction transactions[] = {
+        {"A332131", "Withdraw", 40, ""},
+        {"A342131", "Create", 40, ""},
+        {"A342131", "Inquiry", 0, ""},
+        {"A382131", "Create", 0, ""},
+        {"A382131", "Deposit", 10, ""},
+        {"A342131", "Transfer", 40, "A382131"},
+        {"A342131", "Inquiry", 0, ""},
+        {"A382131", "Inquiry", 0, ""},
+        {"A342131", "Close", 0, ""}
+    };
+    int n = sizeof(transactions) / sizeof(Transaction);
 
-// Function to transfer money between accounts.
-Function Transfer(fromAccountID, toAccountID, amount):
-    If fromAccountID not in AccountInfo or toAccountID not in AccountInfo or AccountsDatabase[fromAccountID] < amount:
-        Print("Insufficient funds or account does not exist.")
-        Return
-    AccountInfo[fromAccountID] -= amount
-    AccountInfo[toAccountID] += amount
-    Print(amount, "transferred from account ID:", fromAccountID, "to account ID:", toAccountID)
+    for (int i = 0; i < n; ++i) {
+        pid_t pid = fork();
 
-// Function to check the balance of an account.
-Function Balance(accountID):
-    If accountID not in AccountInfo:
-        Print("Account does not exist.")
-        Return
-    Print("The balance of account ID:", accountID, "is", AccountInfo[accountID])
+        if (pid == 0) { // Child process
+            processTransaction(transactions[i]);
+            exit(0); // Exit child process
+        } else if (pid > 0) {
+            // Parent process waits for child to complete
+            wait(NULL);
+        } else {
+            printf("Fork failed\n");
+            exit(1);
+        }
+    }
 
-// Function to close an account.
-Function CloseAcc(accountID):
-    If accountID not in AccountsInfo:
-        Print("Account does not exist.")
+    return 0;
+}
+
+void processTransaction(Transaction transaction) {
+    if (strcmp(transaction.transactionType, "Create") == 0) {
+        createAccount(transaction.accountID, transaction.amount);
+    } else if (strcmp(transaction.transactionType, "Deposit") == 0) {
+        deposit(transaction.accountID, transaction.amount);
+    } else if (strcmp(transaction.transactionType, "Withdraw") == 0) {
+        withdraw(transaction.accountID, transaction.amount);
+    } else if (strcmp(transaction.transactionType, "Transfer") == 0) {
+        transfer(transaction.accountID, transaction.targetAccountID, transaction.amount);
+    } else if (strcmp(transaction.transactionType, "Inquiry") == 0) {
+        inquiry(transaction.accountID);
+    } else if (strcmp(transaction.transactionType, "Close") == 0) {
+        closeAccount(transaction.accountID);
+    }
+}
+
+void createAccount(char* accountID, int initialBalance) {
+    char filename[20];
+    sprintf(filename, "%s.txt", accountID);
+    FILE* file = fopen(filename, "w");
+    if (file) {
+        fprintf(file, "%d", initialBalance);
+        fclose(file);
+    } else {
+        printf("Failed to create account for %s\n", accountID);
+    }
+}
+
+void deposit(char* accountID, int amount) {
+    char filename[20];
+    sprintf(filename, "%s.txt", accountID);
+    FILE* file = fopen(filename, "r+");
+    if (file) {
+        int currentBalance;
+        fscanf(file, "%d", &currentBalance);
+        fseek(file, 0, SEEK_SET);
+        fprintf(file, "%d", currentBalance + amount);
+        fclose(file);
+    } else {
+        printf("Account %s not found for deposit\n", accountID);
+    }
+}
+
+void withdraw(char* accountID, int amount) {
+    char filename[20];
+    sprintf(filename, "%s.txt", accountID);
+    FILE* file = fopen(filename, "r+");
+    if (file) {
+        int currentBalance;
+        fscanf(file, "%d", &currentBalance);
+        if (currentBalance >= amount) {
+            fseek(file, 0, SEEK_SET);
+            fprintf(file, "%d", currentBalance - amount);
+        } else {
+            printf("Insufficient funds in account %s\n", accountID);
+        }
+        fclose(file);
+    } else {
+        printf("Account %s not found for withdrawal\n", accountID);
+    }
+}
+
+void transfer(char* accountID, char* targetAccountID, int amount) {
+    // First withdraw from source account
+    withdraw(accountID, amount);
+    // Then deposit to the target account
+    deposit(targetAccountID, amount);
+}
+
+void inquiry(char* accountID) {
+    char filename[20];
+    sprintf(filename, "%s.txt", accountID);
+    FILE* file = fopen(filename, "r");
+    if (file) {
+        int balance;
+        fscanf(file, "%d", &balance);
+        printf("Account %s has balance: $%d\n", accountID, balance);
+        fclose(file);
+    } else {
+        printf("Account %s not found for inquiry\n", accountID);
+    }
+}
+
+void closeAccount(char* accountID) {
+    char filename[20];
+    sprintf(filename, "%s.txt", accountID);
+    if (remove(filename) != 0) {
+        printf("Failed to close account %s\n", accountID);
+    }
+}
+
         Return
     Delete AccountInfo[accountID]
     Print("Account ID:", accountID, "has been closed.")
