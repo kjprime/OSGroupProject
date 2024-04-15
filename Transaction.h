f53602f68118d5730bca573af05c86f8ac942354
Kevin Thomas <123215974+kjprime@users.noreply.github.com>
2024-04-14 22:43:06 -0500

    Update Transaction.h

--- Transaction.h
+++ Transaction.h
@@ -1,3 +1,14 @@
+/* Transaction.h
+Group Members:
+Kevin Thomas, kevin.j.thomas@okstate.edu
+Lucas Sager, lucas.sager@okstate.edu
+Allison Meredith, allison.meredith@okstate.edu
+Group: C
+Author: Kevin Thomas
+Date:4/14/2024
+
+File Discription: struct def for trsaction sepperation
+*/
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
 
